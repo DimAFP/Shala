d0a4d53bc9588edfa6af5fe1f770a6f6801112cf
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-26 23:18:34 +0200

    azeaze

---
+++ Shala/DimeuuuView.swift
@@ -0,0 +1,20 @@
+//
+//  DimeuuuView.swift
+//  Shala
+//
+//  Created by dimitri on 26/05/2022.
+//
+
+import SwiftUI
+
+struct DimeuuuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DimeuuuView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimeuuuView()
+    }
+}
