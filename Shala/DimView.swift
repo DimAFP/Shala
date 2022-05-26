154c56d50f6d342fe897a2aa82d7a377765bd842
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-26 11:14:52 +0200

    dim

---
+++ Shala/DimView.swift
@@ -0,0 +1,20 @@
+//
+//  DimView.swift
+//  Shala
+//
+//  Created by dimitri on 26/05/2022.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text("Dim")
+    }
+}
+
+struct DimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimView()
+    }
+}
