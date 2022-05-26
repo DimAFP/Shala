aede7accbf7e3b6ada1226eada089cb7af5feff3
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 10:49:11 +0200

    first

---
+++ Shala/ShalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShalaApp.swift
+//  Shala
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
